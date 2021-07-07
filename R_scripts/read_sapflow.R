5ac65912e71a0c5ea7d55dc81d38aa5032c0b6f6
Stephanie Pennington <spenn1@terpmail.umd.edu>
2021-07-07 10:38:19 -0400

    Place read_sapflow function in separate script

---
+++ R_scripts/read_sapflow.R
@@ -0,0 +1,22 @@
+# Function to read logger network sapflow data
+# TEMPEST
+# Stephanie Pennington | Created July 2021
+
+# `filename` is a character path to a raw sapflow dataset from a Campbell data logger
+# returns a dataframe
+
+library(readr)
+library(dplyr)
+
+# Function setup
+read_sapflow <- function(filename) {
+    sdat <- readLines(filename)
+    sdat <- sdat[-3:-4] # remove lines 3 and 4 with unneeded information
+
+    # parse line one to extract logger name
+    pnnl_x <- gregexpr("PNNL_", sdat[1])[[1]][1]
+    logger_name <- substr(sdat[1], start = pnnl_x, stop = pnnl_x + 6)
+
+    read_csv(sdat, skip = 1) %>%
+        mutate(Logger = logger_name)
+}
