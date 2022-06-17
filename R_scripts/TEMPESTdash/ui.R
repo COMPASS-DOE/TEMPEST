fd7eca6af25771c6258f2acc9b6111a49a72910a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-06-16 22:05:35 -0400

    Remove commented-out code and reindent

--- R_scripts/TEMPESTdash/ui.R
+++ R_scripts/TEMPESTdash/ui.R
@@ -27,12 +27,12 @@ ui <- dashboardPage(
             menuItem("AquaTroll", tabName = "troll", icon = icon("water")),
             menuItem("Battery", tabName = "battery", icon = icon("car-battery")),
             menuItem("Alerts", tabName = "alerts", icon = icon("comment-dots"))
-            )
+        )
     ),
     dashboardBody(
         tags$head(tags$style(".shiny-notification {position: fixed; top: 30% ;left: 50%; width: 300px")),
         tabItems(
-          tabItem(
+            tabItem(
                 tabName = "dashboard",
                 fluidRow(
                     # Frontpage - boxes
@@ -49,86 +49,47 @@ ui <- dashboardPage(
                         "100%", "Battery",
                         color = "green",
                         icon = icon("car-battery"), width = 3),
-                    ),
+                ),
                 fluidRow(
                     # selectInput(inputId = "plot",
                     #             label = "Plot:",
                     #             choices = c("Control", "Seawater", "Freshwater")),
                     tabBox(width = 12,
                            #uiOutput("plotSelector"),
-                        tabPanel(
-                            title = "Sapflow",
-                            plotlyOutput("sfsensor_timeseries", height = "200px")
-                        ),
-                        tabPanel(
-                            title = "TEROS",
-                            plotlyOutput("teros_timeseries", height = "200px")
-                        ),
-                        tabPanel(
-                            title = "AquaTroll"
-                        ),
-                        tabPanel(
-                            title = "Battery",
-                            plotlyOutput("battery", height = "200px")
-                        )
+                           tabPanel(
+                               title = "Sapflow",
+                               plotlyOutput("sfsensor_timeseries", height = "200px")
+                           ),
+                           tabPanel(
+                               title = "TEROS",
+                               plotlyOutput("teros_timeseries", height = "200px")
+                           ),
+                           tabPanel(
+                               title = "AquaTroll"
+                           ),
+                           tabPanel(
+                               title = "Battery",
+                               plotlyOutput("battery", height = "200px")
+                           )
                     )
                 )
 
-                # fluidRow(
-                #     tabBox(
-                #         width = 12,
-                #         tabPanel(
-                #             status = "primary",
-                #             title = "Sapflow",
-                #             plotOutput("sf_timeseries", height = "800px")
-                #         )
-                #     )
-                # ),
-# actionButton("update", "Update Data"),
-# actionButton("refreshButton",
-#              label = "Refresh",
-#              class = "btn-success"),
+            ),
+            tabItem(
+                tabName = "alerts",
+                textInput(inputId = "phone-number",
+                          label = "Phone number:",
+                          value = "(301) 555-5555"),
 
-),
-tabItem(
-    tabName = "alerts",
-    textInput(inputId = "phone-number",
-              label = "Phone number:",
-              value = "(301) 555-5555"),
+                # Input: Selector for choosing dataset ----
+                selectInput(inputId = "carrier",
+                            label = "Select your carrier:",
+                            choices = c("Verizon", "AT&T", "T Mobile")),
 
-    # Input: Selector for choosing dataset ----
-    selectInput(inputId = "carrier",
-                label = "Select your carrier:",
-                choices = c("Verizon", "AT&T", "T Mobile")),
+                submitButton("Recieve Text Alerts")
+            )
 
-    submitButton("Recieve Text Alerts")
-)
-
-          # tabItem(
-          #        tabName = "sapflow",
-          #       fluidRow(
-          #           box(uiOutput("plotSelector"),
-          #               plotOutput("sf_timeseries", height = "800px"),
-          #               width = 12),
-          #           box(uiOutput("sensorSelector"),
-          #               dygraphOutput("sfsensor_timeseries", height = "800px"),
-          #               width = 12
-          #           ),
-          #           uiOutput("dataloggerSelector"),
-          #           dataTableOutput("table")
-          #           ),
-          # )#,
-          # tabItem(
-          #     tabName = "teros",
-          #    # uiOutput("sensorSelectorT"),
-          #     fluidRow(
-          #         uiOutput("plotSelectorT"),
-          #         box(plotOutput("teros_timeseries", height = "800px"), width = 12),
-          #         uiOutput("dataloggerSelector"),
-          #         dataTableOutput("teros_table")
-          #     )
-          #  )
-         )
-     )
+        )
+    )
 )
 
