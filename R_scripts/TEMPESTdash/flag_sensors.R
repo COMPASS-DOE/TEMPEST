748ea386f92ea010276adfebe6f4066d378661ca
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-06-18 07:34:15 -0400

    Restructure badge color computation
    Needed for the TEROS data, which (awkwardly) are three variables in a single dataset

--- R_scripts/TEMPESTdash/flag_sensors.R
+++ R_scripts/TEMPESTdash/flag_sensors.R
@@ -2,16 +2,30 @@ library(dplyr)
 
 # To be filled in based on real data
 # We could provide defaults, but let the users adjust if they want; bonus time
-TEROS_SWC_RANGE <- c()
-SAPFLOW_RANGE <- c(0.4, 0.7) # roughly the 10%/90% quantiles of the test data
-VOLTAGE_RANGE <- c(12.5, 14.3) # roughly 0.95%/99.5% quantiles of the test data
+# For TEROS, these are all 1%/99% quantiles of test data
+TEROS_RANGE <- tribble(~variable, ~low, ~high,
+                       "EC",      16,   225,
+                       "TSOIL",   15,   18,
+                       "VWC",     2538, 2891)
+SAPFLOW_RANGE <- c(0.4, 0.7) # roughly the 10%/90% quantiles of test data
+VOLTAGE_RANGE <- c(12.5, 14.3) # roughly 0.95%/99.5% quantiles of test data
 
 # Green-yellow-red levels; could have more colors if desired
-STOPLIGHT_COLORS <- c("green" = 0.0,    # green starts at 0% fail (this shouldn't change)
-                      "yellow" = 0.05,  # yellow starts at 5% fail
-                      "red" = 0.2)      # red starts at 20%
-if(STOPLIGHT_COLORS[1] != 0.0) {
-    stop("The first entry in STOPLIGHT_COLOR must be zero")
+BADGE_COLORS <- c("green" = 0.0,    # green starts at 0% fail (this shouldn't change)
+                  "yellow" = 0.05,  # yellow starts at 5% fail
+                  "red" = 0.2)      # red starts at 20%
+
+# Compute badge color(s) based on fraction(s) out
+badge_color <- function(frac_out) {
+    if(BADGE_COLORS[1] != 0.0) {
+        stop("The first entry in BADGE_COLORS must be zero")
+    }
+
+    colors <- cut(frac_out,
+                  c(BADGE_COLORS, 1),
+                  labels = names(BADGE_COLORS),
+                  right = FALSE)
+    as.character(colors)
 }
 
 # Compute fraction (0-1) of values outside specified limits
@@ -23,17 +37,14 @@ frac_outside_limits <- function(values, left_limit, right_limit, na.rm = FALSE) 
     sum(is.na(values) | !between(values, left_limit, right_limit)) / length(values)
 }
 
-# Return both fraction_out and associated stoplight color for a vector of
+# Return both fraction_out and associated badge color for a vector of
 # values and associated limits
 flag_sensors <- function(values, limits, na.rm = FALSE) {
     frac_out <- frac_outside_limits(values, min(limits), max(limits), na.rm = na.rm)
-    colors <- cut(frac_out,
-                  c(STOPLIGHT_COLORS, 1),
-                  labels = names(STOPLIGHT_COLORS),
-                  right = FALSE)
-    tibble(fraction_in = 1- frac_out,
+    tibble(n = length(values),
+           fraction_in = 1- frac_out,
            percent_in = paste0(round(fraction_in * 100, 0), "%"),
-           color = as.character(colors))
+           color = badge_color(frac_out))
 }
 
 #
