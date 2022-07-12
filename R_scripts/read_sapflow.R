566f3a6b4b8a99399bb1ff6e777d9fdb2a4bec2f
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-07-11 22:36:02 -0400

    Use compasstools::read_sapflow_file()

--- R_scripts/read_sapflow.R
+++ R_scripts/read_sapflow.R
@@ -7,6 +7,7 @@
 
 library(readr)
 library(dplyr)
+library(compasstools)
 
 # Function setup
 read_sapflow <- function(filename, token, total_files) {
@@ -21,15 +22,7 @@ read_sapflow <- function(filename, token, total_files) {
                   dtoken = token,
                   overwrite = TRUE)
 
-    sdat <- readLines("tempfile.dat") #temp file goes here
+    sdat <- compasstools::read_sapflow_file("tempfile.dat")
     unlink("tempfile.dat")
-    sdat <- sdat[-3:-4] # remove lines 3 and 4 with unneeded information
-
-    # parse line one to extract logger name
-    pnnl_x <- gregexpr("PNNL_", sdat[1])[[1]][1]
-    logger_name <- substr(sdat[1], start = pnnl_x, stop = pnnl_x + 6)
-
-    # The "I()" notation is how to read from a string; see help page
-    read_csv(I(sdat), skip = 1) %>%
-        mutate(Logger = logger_name)
+    sdat
 }
