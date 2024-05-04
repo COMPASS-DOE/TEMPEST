1aa886197548a8358fde4242a940d6699d797275
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2024-05-04 18:02:08 -0400

    definitely still not working

--- R_scripts/process_treeflux.R
+++ R_scripts/process_treeflux.R
@@ -6,11 +6,8 @@ library(fluxfinder)
 library(dplyr)
 library(lubridate)
 
-#set data path
-setwd("C:/Users/morr497/OneDrive - PNNL/Documents/TEMPEST/TEMPEST/Data/")
-
 # Get names of data files
-files <- list.files("tree_flux_licor/", pattern = "\\.data$", full.names = TRUE)
+files <- list.files("Data/tree_flux_licor/", pattern = "\\.data$", full.names = TRUE)
 
 # Helper function
 read_file <- function(f) {
@@ -26,38 +23,108 @@ lapply(files, read_file) %>%
 tree_data_one <- tree_data_raw[1:650,]
 
 #read in metadata
-meta22 <- read.csv("tree_flux_licor/metadata & excel files/tree_flux_metadata22.csv ")
-meta23 <- read.csv("tree_flux_licor/metadata & excel files/tree_flux_metadata23.csv ")
+meta22 <- read.csv("Data/tree_flux_licor/metadata & excel files/tree_flux_metadata22.csv ")
+meta23 <- read.csv("Data/tree_flux_licor/metadata & excel files/tree_flux_metadata23.csv ")
 
-meta_dat <- bind_rows(meta22, meta23)
-
-meta_dat %>%
+meta22 %>%
     mutate(start_string = paste(collection_date, start_time),
            start_timestamp = as.POSIXct(start_string, format = "%m/%d/%Y %H:%M", tz = "EST"),
+           start_clock = hm(start_time),
            end_string = paste(collection_date, end_time),
            end_timestamp = as.POSIXct(end_string, format = "%m/%d/%Y %H:%M", tz = "EST"),
            collection_date = date(start_timestamp),
-           obs_lengths = 600) -> meta_dat2#%>%
-    #select(-c(start_string, end_string)) -> meta_dat
+           obs_lengths = 600) -> meta_dat22
 
-meta_dat2 %>%
-    filter(collection_date == "2022-06-20") %>%
-    arrange(start_timestamp) -> one_day_meta_dat
+meta23 %>%
+    mutate(start_string = paste(collection_date, start_time),
+           start_timestamp = as.POSIXct(start_string, format = "%m/%d/%Y %H:%M", tz = "EST"),
+           start_timestamp = start_timestamp - hours(1), #2023 metadata recorded in EDT
+           start_clock = hm(start_time),
+           start_clock = start_clock - hours(1),
+           end_string = paste(collection_date, end_time),
+           end_timestamp = as.POSIXct(end_string, format = "%m/%d/%Y %H:%M", tz = "EST"),
+           end_timestamp = end_timestamp - hours(1),
+           collection_date = date(start_timestamp),
+           obs_lengths = 600) -> meta_dat23
 
-#tree_w_meta <-
+#this would be a good place for some defensive programming
+#script should stop is start/end_timestamp and collection_date aren't correctly converted
+
+meta_dat <- bind_rows(meta_dat22, meta_dat23)
+
+
+#create single licor file for testing
+tree_data_raw %>%
+    filter(File == "TG10-01448-2023-06-04T050000_Mike.data") -> mike
+
+#mike_w_meta <-
 ffi_metadata_match(
-    data_timestamps = tree_data_raw$TIMESTAMP,
-    start_dates = meta_dat$start_timestamp,
-    start_times = hm(meta_dat$start_timestamp),
-    obs_lengths = meta_dat$obs_lengths
+    data_timestamps = mike$TIMESTAMP,
+    start_dates = meta_dat[meta_dat$collection_date == "2023-06-04",]$start_timestamp,
+    start_times = meta_dat[meta_dat$collection_date == "2023-06-04",]$start_clock,
+    obs_lengths = meta_dat[meta_dat$collection_date == "2023-06-04",]$obs_lengths
 )
+#returns
+#Error in ffi_metadata_match(data_timestamps = mike$TIMESTAMP, start_dates = meta_dat[meta_dat$collection_date ==  :
+#start_timestamps overlaps: 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 31, 32, 34, 35, 36, 37, 38, 40, 41, 42, 43, 45, 46, 47, 48, 49
 
-#tree_one_w_meta <-
+meta_dat %>%
+    filter(plot == "Freshwater") -> f_dat
+
+#mike_w_meta <-
 ffi_metadata_match(
-    data_timestamps = tree_data_one$TIMESTAMP,
-    start_dates = one_day_meta_dat$start_timestamp,
-    start_times = hm(one_day_meta_dat$start_timestamp),
-    obs_lengths = one_day_meta_dat$obs_lengths
-)
+    data_timestamps = mike$TIMESTAMP,
+    start_dates = f_dat[f_dat$collection_date == "2023-06-04",]$start_timestamp,
+    start_times = f_dat[f_dat$collection_date == "2023-06-04",]$start_clock,
+    obs_lengths = f_dat[f_dat$collection_date == "2023-06-04",]$obs_lengths
+) -> wtf
+#returns a large numeric element and a warning that there are 18 entries with no matches
+#element converted into a dataframe is a single column of NA the length of "mike"
+
+meta_dat %>%
+    filter(plot == "Control") -> c_dat
+
+#mike_w_meta <-
+ffi_metadata_match(
+    data_timestamps = mike$TIMESTAMP,
+    start_dates = c_dat[c_dat$collection_date == "2023-06-04",]$start_timestamp,
+    start_times = c_dat[c_dat$collection_date == "2023-06-04",]$start_clock,
+    obs_lengths = c_dat[c_dat$collection_date == "2023-06-04",]$obs_lengths
+) -> ugh
+#returns a large numeric element and a warning that there are 18 entries with no matches
+#element converted into a dataframe is a single column of NA the length of "mike"
+
+
+meta_dat %>%
+    filter(plot == "Seawater") -> s_dat
+
+#mike_w_meta <-
+ffi_metadata_match(
+    data_timestamps = mike$TIMESTAMP,
+    start_dates = s_dat[s_dat$collection_date == "2023-06-04",]$start_timestamp,
+    start_times = s_dat[s_dat$collection_date == "2023-06-04",]$start_clock,
+    obs_lengths = s_dat[s_dat$collection_date == "2023-06-04",]$obs_lengths
+) -> fingers_crossed
+
+#Error in ffi_metadata_match(data_timestamps = mike$TIMESTAMP, start_dates = s_dat[s_dat$collection_date ==  :
+#  start_timestamps overlaps: 5
+
+
+
+
+
+
+split(tree_data_raw, f = tree_data_raw$File) -> grouped_tree_data
+
+# Helper function
+match_data <- function(m) {
+    message("Matching ", m)
+    ffi_metadata_match(data_timestamps = m$TIMESTAMP,
+                       start_dates = meta_dat$start_timestamp,
+                       start_times = hms(meta_dat$start_timestamp),
+                       obs_lengths = meta_dat$obs_lengths)
+}
+
+lapply(grouped_tree_data, match_data) -> tree_data_matched
 
 #at some point will need to pull air temperature data
