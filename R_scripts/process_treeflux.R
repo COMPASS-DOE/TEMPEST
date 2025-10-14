a974190cb49cae1e32275431f4a93405b355c26b
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2025-10-14 11:29:18 -0400

    walk through with BBL

--- R_scripts/process_treeflux.R
+++ R_scripts/process_treeflux.R
@@ -4,9 +4,12 @@
 
 library(fluxfinder)
 library(dplyr)
+library(stringr)
+library(ggplot2)
 library(lubridate)
 
 # Get names of data files
+# from TEMPEST I and II (2022 and 2023)
 files <- list.files("Data/tree_flux_licor/", pattern = "\\.data$", full.names = TRUE)
 
 # Helper function
@@ -24,8 +27,6 @@ lapply(files, read_file) %>%
 meta22 <- read.csv("Data/tree_flux_licor/metadata & excel files/tree_flux_metadata22.csv ")
 meta23 <- read.csv("Data/tree_flux_licor/metadata & excel files/tree_flux_metadata23.csv ")
 
-library(stringr)
-
 meta22 %>%
     mutate(start_string = paste(collection_date, start_time),
            start_timestamp = as.POSIXct(start_string, format = "%m/%d/%Y %H:%M", tz = "EST"),
@@ -42,8 +43,6 @@ meta23 %>%
            start_timestamp = start_timestamp - hours(1), #2023 metadata recorded in EDT
            start_clock = paste0(start_time, ":00"),
            start_clock = str_pad(start_clock, width=8, side="left", pad="0"),
-           #start_clock = hms(start_clock),
-           #start_clock = start_clock - hours(1),
            end_string = paste(collection_date, end_time),
            end_timestamp = as.POSIXct(end_string, format = "%m/%d/%Y %H:%M", tz = "EST"),
            end_timestamp = end_timestamp - hours(1),
@@ -57,90 +56,126 @@ meta_dat <- bind_rows(meta_dat22, meta_dat23)
 meta_dat %>%
     arrange(start_timestamp) -> meta_dat
 
+# OK, we've got the input files, now what?
 
-#plot one slope
-library(ggplot2)
-
-#create single licor file for testing
+# create single licor file for testing
 tree_data_raw %>%
     filter(File == "TG10-01028-2022-06-19T000000_Nick.data") -> Nick
-
+# check timestamps
 head(Nick$TIMESTAMP)
 
 # filter to one day
 Nick %>%
-    filter(#date(TIMESTAMP) == "2022-06-20"
-           #TIMESTAMP < "2022-06-20 13:10:00 EDT",
-           #TIMESTAMP > "2022-06-20 12:45:00 EDT"
-           ) -> Nick_event1 #pre_treatment
+    filter(date(TIMESTAMP) == "2022-06-20") -> Nick_pre_treatment
 
+# we have data, yes?
 ggplot(Nick_pre_treatment, aes(x = TIMESTAMP, y = CH4)) +
-    geom_point() +
-    ylim(1950, 2050) +
-    ggtitle("Seawater PreTreatment")
+         geom_point() +
+         ylim(1950, 2050) +
+         ggtitle("Seawater PreTreatment 2022")
 
-#filter metadata
+# filter metadata for the same day
 meta_dat %>%
-    filter(year(collection_date) == "2022",
-           plot == "Seawater") -> Event1_seawater
-Event1_seawater <- Event1_seawater[-80,]
-Event1_seawater <- Event1_seawater[-1,]
+    filter(collection_date == "2022-06-20",
+           plot == "Seawater") -> pretreatment_seawater
+# we know it's seawater because we used our human eyes
+# should think of a way to automate that matching
 
+# matchy match?
 Nick_pre_treatment$match <-
 ffi_metadata_match(
     data_timestamps = Nick_pre_treatment$TIMESTAMP,
-    start_dates = as.character(first_day22_seawater$collection_date),
-    start_times = first_day22_seawater$start_clock,
-    obs_lengths = first_day22_seawater$obs_lengths
+    start_dates = as.character(pretreatment_seawater$collection_date),
+    start_times = pretreatment_seawater$start_clock,
+    obs_lengths = pretreatment_seawater$obs_lengths
 )
 
-Nick_event1$match <-
-    ffi_metadata_match(
-        data_timestamps = Nick_event1$TIMESTAMP,
-        start_dates = as.character(Event1_seawater$collection_date),
-        start_times = Event1_seawater$start_clock,
-        obs_lengths = Event1_seawater$obs_lengths
-    )
-
-Nick_event1 %>%
-    filter(! is.na(match)) -> Seawater_Event1_matched
-
-#need to add tree ids to each flux chunk
-Event1_seawater %>%
-    select(ID) %>%
-    mutate(match = 1:142) %>%
-    right_join(Seawater_Event1_matched, by = "match") -> Seawater_Event1_matched
-
-Nick_pre_treatment %>%
-    filter(! is.na(match)) -> matched_example
-
-
-ggplot(matched_example, aes(x = TIMESTAMP, y = CH4, color = as.factor(match))) +
-    geom_point() +
-    ylim(1950, 2050) +
-    ggtitle("Seawater PreTreatment") +
-    facet_wrap(~as.factor(match), scales = "free_x")
-
-
-ggplot(Seawater_Event1_matched[Seawater_Event1_matched$ID == "S1",],
+# we have pretty good match, only missing one!
+ggplot(data = Nick_pre_treatment,
        aes(x = TIMESTAMP, y = CH4)) +
     geom_point() +
+    facet_wrap(.~match, scales = "free") +
     ylim(1950, 2050) +
-    ggtitle("Seawater TEMPEST II") +
-    facet_wrap(~date(TIMESTAMP), scales = "free")
+    ggtitle("Seawater PreTreatment One Day 2022")
+
+# Summary thus far...
+# we have pretty good matching using the default settings
+# and one to one inputs
 
 
-split(tree_data_raw, f = tree_data_raw$File) -> grouped_tree_data
+# let's test another day or two
 
-# Helper function
-match_data <- function(m) {
-    message("Matching ", m)
-    ffi_metadata_match(data_timestamps = m$TIMESTAMP,
-                       start_dates = as.character(meta_dat$collection_date),
-                       start_times = meta_dat$start_clock,
-                       obs_lengths = meta_dat$obs_lengths)
-}
+# create single licor file for testing
+tree_data_raw %>%
+    filter(File == "TG10-01286-2023-06-04T050000_Louise.data") -> Louise
+head(Louise$TIMESTAMP)
 
-lapply(grouped_tree_data, match_data) -> tree_data_raw$matched
+# filter to one day
+Louise %>%
+    filter(date(TIMESTAMP) == "2023-06-04") -> Louise_oneday
 
-#at some point will need to pull air temperature data
+# do we have data?
+ggplot(Louise_oneday, aes(x = TIMESTAMP, y = CH4)) +
+    geom_point() +
+    ylim(1950, 2050) +
+    ggtitle("Louise Pretreatment 2023")
+
+# filter metadata
+meta_dat %>%
+    filter(collection_date == "2023-06-04",
+           plot == "Control") -> pretreatment_control
+# matching!
+Louise_oneday$match <-
+    ffi_metadata_match(
+        data_timestamps = Louise_oneday$TIMESTAMP,
+        start_dates = as.character(pretreatment_control$collection_date),
+        start_times = pretreatment_control$start_clock,
+        obs_lengths = pretreatment_control$obs_lengths
+    )
+
+# did we match?
+ggplot(data = Louise_oneday,
+       aes(x = TIMESTAMP, y = CH4)) +
+    geom_point() +
+    facet_wrap(.~match, scales = "free") +
+    ylim(1950, 2050) +
+    ggtitle("Control PreTreatment One Day 2023")
+
+# second test, passed!
+
+
+# create single licor file for testing
+tree_data_raw %>%
+    filter(File == "TG10-01448-2023-06-04T050000_Mike.data") -> Mike
+tail(Mike$TIMESTAMP)
+
+# filter to one day
+Mike %>%
+    filter(date(TIMESTAMP) == "2023-06-09") -> Mike_oneday
+
+# do we have data?
+ggplot(Mike_oneday, aes(x = TIMESTAMP, y = CH4)) +
+    geom_point() +
+    ylim(1950, 2050) +
+    ggtitle("Mike Posttreatment 2023")
+
+# filter metadata
+meta_dat %>%
+    filter(collection_date == "2023-06-09",
+           plot == "Freshwater") -> posttreatment_fw
+# matching!
+Mike_oneday$match <-
+    ffi_metadata_match(
+        data_timestamps = Mike_oneday$TIMESTAMP,
+        start_dates = as.character(posttreatment_fw$collection_date),
+        start_times = posttreatment_fw$start_clock,
+        obs_lengths = posttreatment_fw$obs_lengths
+    )
+
+# did we match?
+ggplot(data = Mike_oneday,
+       aes(x = TIMESTAMP, y = CH4)) +
+    geom_point() +
+    facet_wrap(.~match, scales = "free") +
+    ylim(1950, 2050) +
+    ggtitle("Mike One Day 2023")
