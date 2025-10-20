dd7e4f902f4b858c85f49dcc3a27c859fb0ac182
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2025-10-20 09:41:50 -0400

    Generalize script

---
+++ Data/tree_flux_licor/README.md
@@ -0,0 +1,9 @@
+# README.md
+
+This folder contains
+
+* Licor output files from 2022 and 2023 stem GHG fluxes measured around the TEMPEST floods;
+* Metadata for those measurement campaigns;
+* Processing info (`treeflux-processing-info.csv`)
+* Outputs and diagnostics (`processing_outputs/`)
+
--- R_scripts/process_treeflux.R
+++ R_scripts/process_treeflux.R
@@ -10,6 +10,7 @@ library(fluxfinder)
 library(dplyr)
 library(stringr)
 library(ggplot2)
+theme_set(theme_bw())
 library(lubridate)
 
 # Get names of data files
@@ -32,15 +33,14 @@ lapply(files, read_file) %>%
 # Read in metadata and construct start/end timestamps
 message("Reading metadata...")
 meta22 <- read_csv("Data/tree_flux_licor/metadata_excel_files/tree_flux_metadata22.csv",
-                   col_types = "cccccccc")
+                   col_types = "ccccccddcc")
 meta23 <- read_csv("Data/tree_flux_licor/metadata_excel_files/tree_flux_metadata23.csv",
-                   col_types = "cccccccc")
+                   col_types = "ccccccddcc")
 
 meta22 %>%
     bind_rows(meta23) %>%
     mutate(start_timestamp = mdy_hm(paste(collection_date, start_time), tz = "EST"),
-           end_timestamp = mdy_hm(paste(collection_date, end_time), tz = "EST"),
-           obs_lengths = 100) %>%
+           end_timestamp = mdy_hm(paste(collection_date, end_time), tz = "EST")) %>%
     select(-start_time, -end_time, -collection_date, -notes) %>%
     arrange(start_timestamp) ->
     md
@@ -55,57 +55,87 @@ if(any(is.na(md$end_timestamp))) {
 
 # OK, we've got the input files, now what?
 
+# We use a "treeflux-processing-info" file to step through the data. This
+# simplifies things and provides a documentary record of decisions, etc.
+message("Reading processing info file...")
+tfpi <- read_csv("Data/tree_flux_licor/treeflux-processing-info.csv", col_types = "cDcc")
+
+i <- 1
+
+FILE <- tfpi$File[i]
+DATE <- tfpi$Date[i]
+PLOT <- tfpi$Plot[i]
+I_STR <- sprintf("%02s", i)
+
+message(paste("Processing", i, FILE, DATE, PLOT))
+
 # Filter to one Licor file and one day for testing
 tree_data_raw %>%
-    filter(File == "TG10-01028-2022-06-19T000000_Nick.data") %>%
-    filter(date(TIMESTAMP) == "2022-06-20") ->
-    Nick_pre_treatment
+    filter(File == FILE) %>%
+    filter(date(TIMESTAMP) == DATE) ->
+    tree_data_filtered
+message("\t", nrow(tree_data_filtered), " rows of data")
 
-# We have data, yes?
-# Here and below, we use CO2 for plotting because we *know* it has to be
-# emitted., not taken up, by tree stems, which makes diagnosing matching
-# problems easier
-ggplot(Nick_pre_treatment, aes(x = TIMESTAMP, y = CO2)) +
+# Here and below, we use CO2 for plotting because we *know* it has
+# to be emitted, not taken up, by tree stems, which makes it easier
+# to diagnose matching problems
+ggplot(tree_data_filtered, aes(x = TIMESTAMP, y = CO2)) +
     geom_point(na.rm = TRUE) +
     ylim(300, 1000) +
     ggtitle("Seawater PreTreatment 2022")
 
 # Filter metadata for the same day
 md %>%
-    filter(date(start_timestamp) == "2022-06-20",
-           plot == "Seawater") %>%
+    filter(date(start_timestamp) == DATE,
+           plot == PLOT) %>%
     mutate(start_times = paste(hour(start_timestamp),
                                minute(start_timestamp),
                                second(start_timestamp), sep = ":")) ->
-    md_pre_seawater
-# We know it's seawater because we used our human eyes
-# should think of a way to automate that matching
+    md_filtered
+message("\t", nrow(md_filtered), " rows of metadata")
+
+TIMEPOINT <- unique(md_filtered$timepoint)
+if(length(TIMEPOINT) > 1) {
+    stop("Hmm, this should not happen!")
+}
 
 # matchy match?
-Nick_pre_treatment$match <-
+message("Matching...")
+tree_data_filtered$match <-
     ffi_metadata_match(
-        data_timestamps = Nick_pre_treatment$TIMESTAMP,
-        start_dates = as.character(date(md_pre_seawater$start_timestamp)),
-        start_times = md_pre_seawater$start_times,
-        obs_lengths = md_pre_seawater$obs_lengths
+        data_timestamps = tree_data_filtered$TIMESTAMP,
+        start_dates = as.character(date(md_filtered$start_timestamp)),
+        start_times = md_filtered$start_times,
+        obs_lengths = md_filtered$obs_length
     )
+tree_data_filtered$ID <- md_filtered$ID[tree_data_filtered$match]
 
-# We have pretty good match, only missing one!
-ggplot(Nick_pre_treatment, aes(x = TIMESTAMP, y = CO2, color = factor(match))) +
+test <- tree_data_filtered %>% left_join(md_filtered, by = "ID")
+
+# Diagnostic plot 1: color data by match
+ggplot(tree_data_filtered, aes(x = TIMESTAMP, y = CO2, color = factor(match))) +
     geom_point(na.rm = TRUE) +
     ylim(300, 1000) +
-    ggtitle("Seawater PreTreatment 2022 - matched")
+    ggtitle(paste(I_STR, PLOT, TIMEPOINT, DATE, "matched"), subtitle = tfpi$Notes[i])
 
-ggplot(data = Nick_pre_treatment,
-       aes(x = TIMESTAMP, y = CO2)) +
+DIR_ROOT <- "Data/tree_flux_licor/processing_outputs/"
+FN_ROOT <- paste(I_STR, FILE, DATE, PLOT, sep = "_")
+ggsave(file.path(DIR_ROOT, paste0(FN_ROOT, "_match.png")), width = 10, height = 6)
+
+# Diagnostic plot 2: individual tree data with
+ggplot(tree_data_filtered, aes(x = TIMESTAMP, y = CO2)) +
     geom_point(na.rm = TRUE) +
-    facet_wrap(.~match, scales = "free") +
+    facet_wrap(. ~ ID, scales = "free_x") +
     ylim(300, 1000) +
-    ggtitle("Seawater PreTreatment 2022 - matched")
+    geom_vline(data = md_filtered,
+               aes(xintercept = start_timestamp + start_adjust),
+               linetype = 2, color = "darkgreen") +
+    geom_vline(data = md_filtered,
+               aes(xintercept = start_timestamp + start_adjust + obs_length),
+               linetype = 2, color = "darkred") +
+    ggtitle(paste(I_STR, PLOT, TIMEPOINT, DATE, "fluxwindows"))
+ggsave(file.path(DIR_ROOT, paste0(FN_ROOT, "_fluxwindows.png")), width = 10, height = 6)
 
-# Summary thus far...
-# We have pretty good matching using the default settings
-# and one to one inputs
 
 stop("All done")
 
